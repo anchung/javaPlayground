7a052b768d14da89fc54921ed60ae57f4f39ead5
A.C <anchung.man@gmail.com>
2020-06-26 15:31:09 -0700

    1. Update ClientSocketExample 2. TimeClientExample

--- src/main/java/com/anchung/javaPlayground/TimeClientExample.java
+++ src/main/java/com/anchung/javaPlayground/TimeClientExample.java
@@ -1,32 +1,25 @@
 package com.anchung.javaPlayground;
 
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientSocketExample {
+public class TimeClientExample {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("www.google.com", 80)) {
+//        try(Socket socket = new Socket("www.google.com", 80)) {
+        try(Socket socket = new Socket("localhost", 13)) {
+            socket.setSoTimeout(5000);
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
 
-            pw.print("GET / HTTP/1.0\r\n");
-            pw.println();
-
             StringBuilder sb = new StringBuilder();
             String line;
-            while ((line = br.readLine()) != null) {
+            while((line = br.readLine()) != null) {
                 sb.append(line);
             }
-
             br.close();
-
-            FileWriter fw = new FileWriter("/tmp/google.html", false);
-            fw.append(sb);
-            fw.flush();
-            fw.close();
+            System.out.println("Server Time: " + sb);
         } catch (Exception e) {
             e.printStackTrace();
         }
